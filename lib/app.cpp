ce761524fe3c69f09de145d1f3afa6523329c435
Alex Hancock <alex@alexhancock.com>
2013-09-02 16:23:15 -0700

    [transformations] Rotate the rect around a center point

--- lib/app.cpp
+++ lib/app.cpp
@@ -34,10 +34,10 @@ static void RenderScene()
 
     // Create a 4x4 matrix to perform a translation transformation
     Matrix4f World;
-    World.m[0][0] = 1.0f; World.m[0][1] = 0.0f; World.m[0][2] = 0.0f; World.m[0][3] = sinf(Scale);
-    World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = sinf(Scale); World.m[1][3] = sinf(Scale);
+    World.m[0][0] = cosf(Scale); World.m[0][1] = -sinf(Scale); World.m[0][2] = 0.0f; World.m[0][3] = 0.0f;
+    World.m[1][0] = sinf(Scale); World.m[1][1] = cosf(Scale); World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
     World.m[2][0] = 0.0f; World.m[2][1] = 0.0f; World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
-    World.m[3][0] = 0.0f; World.m[3][1] = sinf(Scale); World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
+    World.m[3][0] = 0.0f; World.m[3][1] = 0.0f; World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
 
     // Load the matrix into the shader
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
@@ -76,8 +76,8 @@ int main(int argc, char** argv)
     // TODO - Abstract drawing logic into a class
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
-    glutInitWindowSize(1024, 768);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(1920, 1080);
+    glutInitWindowPosition(10, 10);
     glutCreateWindow("Rectangles");
 
     InitializeGlutCallbacks();
